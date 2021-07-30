2eab67bd5db4be38a467712c8bf85f57ffe56ee9
Priyabrat <priyabratswain888@gmail.com>
2021-07-30 23:22:35 +0530

    Initial Commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/blog',{
+mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
